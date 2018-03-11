d596d493f763d798d66239014353b78dbea06d26
salva <salvadj1@hotmail.com>
2018-03-11 14:11:25 +0100

    Timer Fix

--- LootSpawner/Loot.cs
+++ LootSpawner/Loot.cs
@@ -11,25 +11,71 @@ namespace LootSpawner
 {
     public class Loot : MonoBehaviour
     {
-		internal bool IsRunning = false;
-		
+        //internal bool IsRunning = false;
+        internal bool LoadupLootEnabled = true;
+
         void Start()
         {
-            StartC();
+            //StartC();
+            StartCoroutine(LoadupLoot());
         }
-        
+
         // Based on Salva's idea.
         IEnumerator LoadupLoot()
         {
-            //yield return new WaitForSeconds(120);
-
             foreach (var x in LootSpawner.LootPositions.Keys)
             {
-                var obj = Util.GetUtil().FindClosestEntity(x, 1.5f);
+                if (LoadupLootEnabled)
+                {
+                    var obj = Util.GetUtil().FindClosestEntity(x, 1.5f);
+                    if (obj != null && obj.Object is LootableObject)
+                    {
+                        Util.GetUtil().DestroyObject(((LootableObject)obj.Object).gameObject);
+                    }
+                    yield return new WaitForSeconds(1);
+                    var tempvector = x;
+                    tempvector.y = tempvector.y - 1.6f;
+                    World.GetWorld().Spawn(LootSpawner.GetPrefab(LootSpawner.LootPositions[x]), tempvector);
+                }
+            }
+            if (LootSpawner.Announce && LoadupLootEnabled)
+            {
+                Fougerite.Server.GetServer().Broadcast(LootSpawner.orange + LootSpawner.AnnounceMSG);
+                yield return new WaitForSeconds(LootSpawner.Time * 60); // 20mins * 60 = 1800
+            }
+            //StartC();
+            yield return new WaitForSeconds(10);
+            StartCoroutine(LoadupLoot());
+        }
+        /*
+        public void StopC()
+        {
+            IsRunning = false;
+            StopCoroutine(LoadupLoot());
+        }
+
+        public void StartC()
+        {
+            IsRunning = true;
+            StartCoroutine(LoadupLoot());
+        }*/
+
+        public void SpawnLootsMono()
+        {
+            StartCoroutine(SpawnLootsMonoIE());
+        }
+        IEnumerator SpawnLootsMonoIE()
+        {
+            foreach (var xx in LootSpawner.LootPositions.Keys)
+            {
+                var obj = Util.GetUtil().FindClosestEntity(xx, 1.5f);
                 if (obj != null && obj.Object is LootableObject)
                 {
-                    Util.GetUtil().DestroyObject(((LootableObject) obj.Object).gameObject);
+                    Util.GetUtil().DestroyObject(((LootableObject)obj.Object).gameObject);
                 }
+            }
+            foreach (var x in LootSpawner.LootPositions.Keys)
+            {
                 yield return new WaitForSeconds(1);
                 var tempvector = x;
                 tempvector.y = tempvector.y - 1.6f;
@@ -39,45 +85,6 @@ namespace LootSpawner
             {
                 Fougerite.Server.GetServer().Broadcast(LootSpawner.orange + LootSpawner.AnnounceMSG);
             }
-            yield return new WaitForSeconds(LootSpawner.Time * 60); // 20mins * 60 = 1800
-            StartC();
-        }
-
-        public void StopC()
-        {
-			IsRunning = false;
-            StopCoroutine(LoadupLoot());
-        }
-		
-		public void StartC() 
-		{
-			IsRunning = true;
-			StartCoroutine(LoadupLoot());
-		}
-
-        public void SpawnLootsMono()
-        {
-            StartCoroutine(SpawnLootsMonoIE());
-        }
-        IEnumerator SpawnLootsMonoIE()
-        {
-            foreach (var x in LootSpawner.LootPositions.Keys)
-            {
-                var obj = Util.GetUtil().FindClosestEntity(x, 1.5f);
-                if (obj != null && obj.Object is LootableObject)
-                {
-                    Util.GetUtil().DestroyObject(((LootableObject)obj.Object).gameObject);
-                }
-                var tempvector = x;
-                tempvector.y = tempvector.y - 1.6f;
-                World.GetWorld().Spawn(LootSpawner.GetPrefab(LootSpawner.LootPositions[x]), tempvector);
-            }
-
-            if (LootSpawner.Announce)
-            {
-                Fougerite.Server.GetServer().Broadcast(LootSpawner.orange + LootSpawner.AnnounceMSG);
-            }
-            yield return new WaitForSeconds(1);
         }
     }
 }
